97b5f4da20dd3a1581aa7fce610f3ff1fa300675
BasketBandit <errorsfound@hotmail.co.jp>
2020-04-05 00:32:12 +0100

    implemented JFugue (good midi player)

--- src/main/java/com/basketbandit/thicctext/ThiccText.java
+++ src/main/java/com/basketbandit/thicctext/ThiccText.java
@@ -3,6 +3,7 @@ package com.basketbandit.thicctext;
 import com.basketbandit.thicctext.io.RequestHandler;
 import com.google.gson.JsonObject;
 import okhttp3.Credentials;
+import org.jfugue.player.Player;
 
 import java.io.File;
 
@@ -12,6 +13,10 @@ public class ThiccText {
         String apiKey = Credentials.basic("apikey", args[1]);
         File text = new File(ThiccText.class.getResource("/text.json").getFile());
 
-        JsonObject object = new RequestHandler(apiUrl, apiKey, text).getJsonObject();
+        // JsonObject object = new RequestHandler(apiUrl, apiKey, text).getJsonObject(); // comment out if testing player to save api calls
+
+        Player player = new Player();
+        player.play("V0 I[Piano] Eq Fh. | Eq Fh. | Fq Eq Dq EqQQ" +
+                "           V1 I[Flute] Rw     | Rw     | EmajQQQ FmajQQQQQ"); // interesting formatting, could be useful for me
     }
 }
