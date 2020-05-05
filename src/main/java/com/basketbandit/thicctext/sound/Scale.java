9fee395747e8776c8aeb16e7797ee3ae5f6fe1cc
BasketBandit <errorsfound@hotmail.co.jp>
2020-05-05 13:11:08 +0100

    added additional scales

--- src/main/java/com/basketbandit/thicctext/sound/Scale.java
+++ src/main/java/com/basketbandit/thicctext/sound/Scale.java
@@ -12,6 +12,7 @@ public class Scale {
         tones.put("Anger", anger);
 
         ArrayList<String[]> fear = new ArrayList<>();
+        fear.add(new String[]{"C4", "D4", "Eb4", "F4", "G4", "Ab5", "Bb5", "C5"});
         tones.put("Fear", fear);
 
         ArrayList<String[]> joy = new ArrayList<>();
@@ -19,15 +20,19 @@ public class Scale {
         tones.put("Joy", joy);
 
         ArrayList<String[]> sadness = new ArrayList<>();
+        sadness.add(new String[]{"C4", "D4", "Eb4", "F4", "G4", "Ab5", "Bb5", "C5"});
         tones.put("Sadness", sadness);
 
         ArrayList<String[]> analytical = new ArrayList<>();
+        analytical.add(new String[]{"C4", "D4", "F4", "G4", "C5"});
         tones.put("Analytical", analytical);
 
         ArrayList<String[]> confident = new ArrayList<>();
+        confident.add(new String[]{"C4", "D4", "E4", "F4", "G4", "A5", "B5", "C5"});
         tones.put("Confident", confident);
 
         ArrayList<String[]> tentative = new ArrayList<>();
+        tentative.add(new String[]{"C4", "D4", "F4", "G4", "C5"});
         tones.put("Tentative", tentative);
     }
 
